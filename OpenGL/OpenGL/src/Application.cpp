8367d2c3e80eb44e82147d66e2ea40835d83f733
Ignacio Leiva <leiva_tato@hotmail.com>
2019-10-13 17:34:50 -0300

    Index buffers

--- OpenGL/OpenGL/src/Application.cpp
+++ OpenGL/OpenGL/src/Application.cpp
@@ -105,22 +105,35 @@ int main(void)
 		cout << "Error al inicialisar glew" << endl;
 
 	//array de vertices para el buffer
-	float positions[6] = {
-		-0.5f, -0.5f,
-		 0.0f,  0.5f,
-		 0.5f, -0.5f
+	float positions[] = {
+		-0.5f, -0.5f,//0
+		 0.5f, -0.5f,//1
+		 0.5f,  0.5f,//2
+
+		 //0.5f,  0.5f,
+		-0.5f,  0.5f,//3
+		//-0.5f, -0.5f
 	};
 
+	unsigned int indices[] = {
+		0, 1, 2,
+		2, 3, 0
+	};
 	//Vertex buffer datos que le pasamos a la VRAM para despues dibujar
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
 	//Vertex attribute es el layout del buffer, es para que el programa sepa que es lo que le estoy mandando 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
+	//Index buffer
+	unsigned int ibo;
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 	//crear shader
 	//string vertexShader =
 	//	"#version 330 core\n"
@@ -162,7 +175,8 @@ int main(void)
 		*/
 
 		/*Draw call*/
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
